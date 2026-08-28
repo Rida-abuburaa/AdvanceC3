df2b57f1fa7c5db3058247af0f98a25b199814a8
Rida Abuburaa <ridaabuburaa@gmail.com>
2026-08-28 13:35:24 -0700

    Question1

--- AdvanceC3/Program.cs
+++ AdvanceC3/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace AdvanceC3
+﻿using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AdvanceC3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            #region Qu1
+            //  Q1: What is a generic class? Why use generics?
+            //A generic class is a class that can work with different data types without rewriting the class for each type.
+            //Why use generics?
+            //Generics are useful because they:
+            // Provide type safety — incorrect types can be detected at compile time.
+            // Reduce code duplication — one class or method can work with many data types.
+            // Avoid unnecessary type casting — retrieved values already have the correct type.
+            // Make code reusable and easier to maintain.
+       
+            #endregion
 
 
         }
